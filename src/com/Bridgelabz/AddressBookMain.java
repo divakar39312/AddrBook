067220a43dc4d0ad1d4efcf2e777f812879c2032
Divakar <divakar39312@gmail.com>
2022-08-16 21:54:25 +0530

    [Divakar]:uc2 file added

--- src/com/Bridgelabz/AddressBookMain.java
+++ src/com/Bridgelabz/AddressBookMain.java
@@ -1,23 +1,49 @@
 package com.Bridgelabz;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class AddressBookMain {
 
-	public static void main(String[] args) {
+	private AddressBook addressBook;
+
+    public AddressBookMain(){
+        addressBook = new AddressBook();
+    }
+    public void addContact(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first and last name : ");
+        String firstName = sc.next();
+        String lastName = sc.next();
+        System.out.println("Enter street : ");
+        String street = sc.next();
+        System.out.println("Enter city and state : ");
+        String city = sc.next();
+       
+        String state = sc.next();
+        System.out.println("Enter zip : ");
+        int zip = sc.nextInt();
+        System.out.println("Enter contact number : ");
+        long phoneNo = sc.nextLong();
+        System.out.println("Enter email id : ");
+        String email = sc.next();
+
+        addressBook.createContact(firstName,lastName,street,city,state,zip,phoneNo,email);
+    }
+    public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program");
 
-        AddressBook addressBook = new AddressBook();
+        AddressBookMain addressBookMain = new AddressBookMain();
+        addressBookMain.addContact();
+        
 
-
-      }
+    }
 	}
-	class AddressBook {
+class AddressBook {
+ArrayList<Contact> contacts = new ArrayList<>();
 
-	    ArrayList<Contact> contacts = new ArrayList<>();
-
-	    public void createContact(String firstName, String lastName, String street, String city, String state, int zip, long phoneNo, String email){
-	        Contact contact = new Contact(firstName,lastName,street,city,state,zip,phoneNo,email);
-	        contacts.add(contact);
-	    }
+public void createContact(String firstName, String lastName, String street, String city, String state, int zip, long phoneNo, String email){
+    Contact contact = new Contact(firstName,lastName,street,city,state,zip,phoneNo,email);
+    contacts.add(contact);
+}
 	}
